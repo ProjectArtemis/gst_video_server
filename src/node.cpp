a9e17fce362623607f30b62f669f9c0934bdb5d6
Vladimir Ermakov <vooon341@gmail.com>
2015-12-03 13:37:14 +0300

    add check that server is loaded

--- src/node.cpp
+++ src/node.cpp
@@ -6,13 +6,21 @@ int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "gst_video");
 
-	// XXX TODO(vooon): check remappings and argv code
 	nodelet::Loader manager(false);
-	nodelet::M_string remappings;
+	nodelet::M_string remappings(ros::names::getRemappings());
 	nodelet::V_string my_argv;
+	const auto node_name = ros::this_node::getName();
 
 	// load server nodelet
-	manager.load(ros::this_node::getName() + "_server", "gst_video_server/server", remappings, my_argv);
+	manager.load(node_name, "gst_video_server/server", remappings, my_argv);
+
+	// code from bebop_autonomy package
+	const auto loaded_nodelets = manager.listLoadedNodelets();
+	if (std::find(loaded_nodelets.begin(), loaded_nodelets.end(), node_name)
+			== loaded_nodelets.end()) {
+		ROS_FATAL("Can not load gst_video_server/server nodelet!");
+		return 1;
+	}
 
 	ros::spin();
 	return 0;
